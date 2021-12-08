fc05fabe334a6062163f346e88af304795844ce2
KatieGott <Katie.Gott@target.com>
2021-12-08 09:28:46 -0600

    first way using iterating slices

---
+++ src/chapter7/main.go
@@ -0,0 +1,31 @@
+package chapter7
+
+import (
+	"fmt"
+	"log"
+)
+
+func main() {
+	lines, err := GetStrings("votes.txt")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	var names []string
+	var counts []int
+	for _, line := range lines {
+		matched := false
+		for i, name := range names {
+			if name == line {
+				counts [i]++
+				matched = true
+			}
+		}
+		if matched == false {
+			names = append(names, line)
+			counts = append(counts, 1)
+		}
+	}
+	for i, name := range names{
+		fmt.Printf("%s: %d\n", name, counts[i])
+	}
+}
