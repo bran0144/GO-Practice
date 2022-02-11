605f81d8b4d039bb7a9a9c6446c4c0a496bad436
KatieGott <Katie.Gott@target.com>
2022-02-11 15:17:19 -0600

    more getters and setters

--- src/chapter10/Coordinates.go
+++ src/chapter10/Coordinates.go
@@ -1,23 +1,50 @@
 package chapter10
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"log"
+)
 
 type Coordinates struct {
-	Latitude float64
-	Longitude float64
+	latitude float64
+	longitude float64
 }
 
-func (c Coordinates) SetLatitude(latitude float64) {
-	c.Latitude = latitude
+//Getters
+func (c *Coordinates) Latitude() float64 {
+	return c.latitude
+}
+func (c *Coordinates) Longitude() float64 {
+	return c.longitude
 }
 
-func (c Coordinates) SetLongitude(longitude float64) {
-	c.Longitude = longitude
+func (c Coordinates) SetLatitude(latitude float64) error {
+	if latitude < -90 || latitude > 90 {
+		return errors.New("invalid latitude")
+	}
+	c.latitude = latitude
+	return nil
+}
+
+func (c Coordinates) SetLongitude(longitude float64) error{
+	if longitude < -180 || longitude > 180 {
+		return errors.New("invalid longitude")
+	}
+	c.longitude = longitude
+	return nil
 }
 
 func main() {
 	coordinates := Coordinates{}
-	coordinates.SetLatitude(37.42)
-	coordinates.SetLongitude(-122.08)
-	fmt.Println(coordinates)
+	err := coordinates.SetLatitude(37.42)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = coordinates.SetLongitude(-122.08)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(coordinates.latitude)
+	fmt.Println(coordinates.longitude)
 }
--- src/chapter10/calendar/date.go
+++ src/chapter10/calendar/date.go
@@ -1,23 +1,30 @@
-package chapter10
+package calendar
 
 import (
 	"errors"
-	"fmt"
-	"log"
 )
 
 type Date struct {
-	Year int
-	Month int
-	Day int
+	year int
+	month int
+	day int
 }
-
+//Getter methods (GO leaves off the Get in the name)
+func (d *Date) Year() int{
+	return d.year
+}
+func (d *Date) Month() int{
+	return d.month
+}
+func (d *Date) Day() int{
+	return d.day
+}
 //Setter methods need pointer receivers (don't need to do anything to d
 func (d *Date) SetYear(year int) error {
 	if year < 1 {
 		return errors.New("invalid year")
 	}
-	d.Year = year
+	d.year = year
 	return nil
 }
 
@@ -25,7 +32,7 @@ func (d *Date) SetMonth(month int) error {
 	if month < 1 || month > 12 {
 		return errors.New("invalid month")
 	}
-	d.Month = month
+	d.month = month
 	return nil
 }
 
@@ -33,27 +40,7 @@ func (d *Date) SetDay(day int) error {
 	if day < 1 || day > 31 {
 		return errors.New("invalid day")
 	}
-	d.Day = day
+	d.day = day
 	return nil
 }
 
-func main() {
-	date := Date{}
-	err := date.SetYear(0)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	err1 := date.SetMonth(5)
-	if err1 != nil {
-		log.Fatal(err1)
-	}
-	date.SetDay(27)
-	err2 := date.SetDay(5)
-	if err2 != nil {
-		log.Fatal(err2)
-	}
-	fmt.Println(date.Year)
-	fmt.Println(date.Month)
-	fmt.Println(date.Day)
-}
