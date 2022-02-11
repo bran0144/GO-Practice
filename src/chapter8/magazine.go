825eecf9fd5ed30c035384d98536868f54bb13d3
KatieGott <Katie.Gott@target.com>
2022-02-11 09:25:10 -0600

    using anonymous field and embedded structs

--- src/chapter8/magazine.go
+++ src/chapter8/magazine.go
@@ -6,10 +6,26 @@ type Subscriber struct {
 	Name   string
 	Rate   float64
 	Active bool
+	Address
 }
 
 //Using a struct literal instead
 //subscriber := magazine.Subscriber(Name: "John Smith", RateL 4.99, Active: true)
+
+type Employee struct{
+	Name string
+	Salary float64
+	Address
+//	Can also use anonymous fields and just do Address (no variable)
+}
+
+type Address struct {
+	Street   string
+	City     string
+	State    string
+	PostCode string
+}
+
 func printInfo(s *Subscriber) {
 	fmt.Println("Name:", s.Name)
 	fmt.Println("Monthly rate:", s.Rate)
@@ -29,7 +45,10 @@ func applyDiscount(s *Subscriber) {
 }
 
 func main() {
+	address := Address{Street: "123 Oak St", City: "Omaha", State: "NE", PostCode: "68111"}
 	subscriber1 := defaultSubscriber("Aman Signh")
+	//or you can do this: subscriber1.HomeAddress.City = "Omaha" to set values
+	subscriber1.Address = address
 	applyDiscount(subscriber1)
 	printInfo(subscriber1)
 
@@ -38,4 +57,15 @@ func main() {
 	//var s subscriber
 	//applyDiscount(&s)
 	//fmt.Println(s.rate)
+
+	var employee Employee
+	employee.Name = "Joy Barr"
+	employee.Salary = 60000
+	employee.Street = "456 Elm St"
+	employee.City = "Minneapolis"
+	employee.State = "MN"
+	employee.PostCode = "55455"
+
+
+
 }
