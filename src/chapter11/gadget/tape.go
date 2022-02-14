38fae32b77feaaf4fed0cc6ec6a25f30f18d4b18
KatieGott <Katie.Gott@target.com>
2022-02-14 09:31:52 -0600

    starting chapter 11

---
+++ src/chapter11/gadget/tape.go
@@ -0,0 +1,27 @@
+package gadget
+
+import "fmt"
+
+type TapePlayer struct {
+	Batteries string
+}
+
+func (t TapePlayer) Play(song string) {
+	fmt.Println("playing", song)
+}
+func (t TapePlayer) Stop() {
+	fmt.Println("stop!")
+}
+type TapeRecorder struct {
+	Microphones int
+}
+func (t TapeRecorder) Play(song string){
+	fmt.Println("playing", song)
+}
+func (t TapeRecorder) Record() {
+	fmt.Println("recording")
+}
+
+func (t TapeRecorder) Stop() {
+	fmt.Println("stop!")
+}
