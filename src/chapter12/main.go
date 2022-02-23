5e13b6f778764171759b93c89f2cd938ca2f30bd
KatieGott <Katie.Gott@target.com>
2022-02-23 15:26:12 -0600

    using recursion

--- src/chapter12/main.go
+++ src/chapter12/main.go
@@ -1,2 +1,20 @@
 package chapter12
 
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+)
+
+func main() {
+	files, err := ioutil.ReadDir("my_direcoty")
+	if err != nil{
+		log.Fatal(err)
+	}
+	for _, file := range files{
+		if file.IsDir() {
+			fmt.Println("Directory:", file.Name())
+		} else{
+			fmt.Println("File:", file.Name())
+		}
+}
--- src/chapter12/my_directory/files.go
+++ src/chapter12/my_directory/files.go
@@ -4,18 +4,33 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"path/filepath"
 )
 
-func main() {
+func scanDirectory(path string) error {
+	fmt.Println(path)
 	files, err := ioutil.ReadDir("my_directory")
 	if err != nil {
 		log.Fatal(err)
 	}
 	for _, file := range files {
+		filePath := filepath.Join(path, file.Name())
 		if file.IsDir() {
-			fmt.Println("Directory:", file.Name())
-		}else {
-			fmt.Println("File:", file.Name())
+			err := scanDirectory(filePath)
+			if err != nil {
+				return err
+			}
+		} else {
+			fmt.Println(filePath)
 		}
 	}
+	return nil
+}
+
+func main() {
+	err := scanDirectory("go")
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
