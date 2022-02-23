c2abce66214a81e9c1722fe0e19a95b5633f96ff
KatieGott <Katie.Gott@target.com>
2022-02-23 15:27:35 -0600

    using panic instead of log.Fatal

--- src/chapter12/my_directory/files.go
+++ src/chapter12/my_directory/files.go
@@ -3,34 +3,26 @@ package my_directory
 import (
 	"fmt"
 	"io/ioutil"
-	"log"
 	"path/filepath"
 )
 
-func scanDirectory(path string) error {
+func scanDirectory(path string) {
 	fmt.Println(path)
 	files, err := ioutil.ReadDir("my_directory")
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 	for _, file := range files {
 		filePath := filepath.Join(path, file.Name())
 		if file.IsDir() {
-			err := scanDirectory(filePath)
-			if err != nil {
-				return err
-			}
+			scanDirectory(filePath)
 		} else {
 			fmt.Println(filePath)
 		}
 	}
-	return nil
 }
 
 func main() {
-	err := scanDirectory("go")
-	if err != nil {
-		log.Fatal(err)
-	}
+	scanDirectory("go")
 
 }
