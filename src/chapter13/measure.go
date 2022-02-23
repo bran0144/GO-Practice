4b8edfca1cac4dc8516c07e914694fda11116aea
KatieGott <Katie.Gott@target.com>
2022-02-23 16:02:47 -0600

    starting chapter 13

---
+++ src/chapter13/measure.go
@@ -0,0 +1,27 @@
+package chapter13
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func main() {
+	responseSize("https://example.com")
+	responseSize("https://golang.org")
+	responseSize("https://golang.org/doc")
+}
+
+func responseSize(url string) {
+	response, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer response.Body.Close()
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(len(body))
+}
