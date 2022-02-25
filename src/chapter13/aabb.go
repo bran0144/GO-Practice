e42531d6bede135cbaf030949e93ee6d5178a951
KatieGott <Katie.Gott@target.com>
2022-02-25 10:09:24 -0600

    sample goroutine

---
+++ src/chapter13/aabb.go
@@ -0,0 +1,21 @@
+package chapter13
+
+import "fmt"
+
+func a() {
+	for i := 0; i < 50; i++ {
+		fmt.Print("a")
+	}
+}
+
+func b() {
+	for i := 0; i < 50; i++ {
+		fmt.Print("b")
+	}
+}
+
+func main() {
+	a()
+	b()
+	fmt.Println("end main")
+}
