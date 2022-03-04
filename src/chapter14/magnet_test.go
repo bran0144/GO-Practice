aa76f486a3ee832019685cb347f1ec61b8d0140d
KatieGott <Katie.Gott@target.com>
2022-03-04 14:12:22 -0600

    more tests

--- src/chapter14/magnet_test.go
+++ src/chapter14/magnet_test.go
@@ -9,7 +9,7 @@ func TestFirstLarger(t *testing.T) {
 	want := 2
 	got := Larger(2, 1)
 	if got != want {
-		t.Error(errorString)
+		t.Error(errorString2(2, 1, got, want))
 	}
 }
 
@@ -17,6 +17,10 @@ func TestSecondLarger(t *testing.T) {
 	want := 8
 	got := Larger(4, 8)
 	if got != want {
-		t.Error(errorString)
+		t.Error(errorString2(4, 8, got, want))
 	}
+}
+
+func errorString2(a int, b int, got int, want int) string {
+	return fmt.Sprintf("Larger(%d, %d) = %d, want %d", a, b, got, want)
 }
