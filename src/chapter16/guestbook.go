04570ebe6b2c205a60a69aadaf776dd0cefc092c
KatieGott <Katie.Gott@target.com>
2022-03-07 15:48:14 -0600

    completed chapter 16

--- src/chapter16/guestbook.go
+++ src/chapter16/guestbook.go
@@ -47,9 +47,27 @@ func viewHandler(writer http.ResponseWriter, request *http.Request) {
 	err = html.Execute(writer, guestbook)
 	check(err)
 }
+func newHandler(writer http.ResponseWriter, request *http.Request) {
+	html, err := template.ParseFiles("new.html")
+	check(err)
+	err = html.Execute(writer, nil)
+	check(err)
+}
+func createHandler(writer http.ResponseWriter, request *http.Request) {
+	signature := request.FormValue("signature")
+	options := os.O_WRONLY | os.O_APPEND | os.O_CREATE
+	file, err := os.OpenFile("signatures.txt", options, os.FileMode(0600))
+	check(err)
+	_, err = fmt.Fprintln(file, signature)
+	check(err)
+	err = file.Close()
+	check(err)
+	http.Redirect(writer, request, "/guestbook", http.StatusFound)
+}
 
 func main() {
 	http.HandleFunc("/guestbook", viewHandler)
+	http.HandleFunc("/guestbook/new", newHandler)
 	err := http.ListenAndServe("localhost:8080", nil)
 	log.Fatal(err)
 }
